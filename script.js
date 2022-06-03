303149d4bd72c6104cac813141bedd8dd503d16b
Andrei Chetan <98164025+AndreiChetan@users.noreply.github.com>
2022-06-03 11:09:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+
+// Disable/Enable Button
+
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// Passing Joke to VoiceRSS API
+
+function tellMe(joke) {
+  VoiceRSS.speech({
+    key: "35e2cf593f5a450180746ca08b87f8c0",
+    src: joke,
+    hl: "en-us",
+    v: "John",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
+// Get Jokes from Joke API
+
+async function getJokes() {
+  let joke = "";
+
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Text-toSpeech
+    tellMe(joke);
+    // Disable Button
+    toggleButton();
+  } catch (error) {
+    // Catch Errors Here
+    console.log("Whoops, error");
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
